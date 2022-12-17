825e75fd10939b65a885f6198e60b6bdd774617b
bum1235aa <63985987+bum1235aa@users.noreply.github.com>
2022-12-17 20:39:39 +0700

    fix ui

--- src/main/java/tdtu/java/controllers/BookController.java
+++ src/main/java/tdtu/java/controllers/BookController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import tdtu.java.services.BookService;
 
@@ -19,8 +20,8 @@ public class BookController {
 //		modelMap.put("book",bookService.findAll());
 //		return "Book";
 //	}
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String bookPage() {
-		return "book";
+		return "home";
 	}
 }
